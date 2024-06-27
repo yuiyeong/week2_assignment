ee970dbbb39e80fcb3f95f5ed0a37f368bc66be1
yuiyeong <joyuiyeong@gmail.com>
2024-06-27 20:50:07 +0900

    Feat: LectureSessionService::enroll 구현 및 LectureSessionController 에서 호출하도록 구현

--- src/main/kotlin/com/yuiyeong/lectureenroll/service/LectureSessionService.kt
+++ src/main/kotlin/com/yuiyeong/lectureenroll/service/LectureSessionService.kt
@@ -16,4 +16,16 @@ class LectureSessionService(
     private val enrollmentRepository: EnrollmentRepository,
 ) {
     fun enroll(lectureSessionId: Long, studentId: Long): LectureSession {
-    }}
+        val session = sessionRepository.findOneById(lectureSessionId) ?: throw LectureSessionNotFoundException()
+        val student = studentRepository.findOneById(studentId) ?: throw StudentNotFoundException()
+        enrollmentRepository.findAllByStudentId(student.id).also {
+            session.addEnrollments(it)
+        }
+
+        val at = LocalDateTime.now()
+        val enrollment = session.enroll(student, at)
+        enrollmentRepository.save(enrollment)
+
+        return sessionRepository.save(session)
+    }
+}
