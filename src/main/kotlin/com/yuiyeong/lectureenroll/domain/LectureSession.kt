995fd5b8af717e8fc1d919c5b9bd0759c7d910c1
yuiyeong <joyuiyeong@gmail.com>
2024-06-27 20:17:59 +0900

    Feat: LectureSession 의 enroll 구현

--- src/main/kotlin/com/yuiyeong/lectureenroll/domain/LectureSession.kt
+++ src/main/kotlin/com/yuiyeong/lectureenroll/domain/LectureSession.kt
@@ -18,4 +18,49 @@ class LectureSession(
         private set
 
     private var enrollments: MutableList<Enrollment> = arrayListOf() // 수강 완료 정보
+
+    init {
+        if (enrollmentPeriod.overlaps(lecturePeriod))
+            throw PeriodOverlapException("신청 기간과 강의 시간을 겹칠 수 없습니다.")
+    }
+
+    fun addEnrollments(enrollments: List<Enrollment>) {
+        this.enrollments.addAll(enrollments)
+    }
+
+    fun enrollments(): List<Enrollment> = enrollments
+
+
+    fun enroll(student: Student, at: LocalDateTime): Enrollment {
+        checkEnrollmentPeriod(at)
+        checkEnrollments(student)
+        checkAvailableCapacity()
+
+        availableCapacity--
+
+        return Enrollment(
+            id = 0L,
+            student = student,
+            lectureSession = this,
+            enrolledAt = at
+        ).also {
+            enrollments.add(it)
+        }
+    }
+
+    private fun checkEnrollmentPeriod(moment: LocalDateTime) {
+        if (!enrollmentPeriod.contains(moment))
+            throw OutOfPeriodException("현재는 신청 기한이 아닙니다.")
+    }
+
+    private fun checkAvailableCapacity() {
+        if (availableCapacity == 0)
+            throw CapacityExceededException("정원이 마감된 강의입니다.")
+    }
+
+    private fun checkEnrollments(student: Student) {
+        val alreadyEnrolled = enrollments.any { it.student == student }
+        if (alreadyEnrolled)
+            throw AlreadyEnrolledException("이미 신청 완료한 강의입니다.")
+    }
 }
